68384ee12128dfd27d68bdc81d58cb3ff8d872b1
Nathan Chow <emailme@nathanchow.ca>
2022-12-29 16:16:36 -0500

    update stories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import Stories from '../src/components/stories';
 
 function App() {
   return (
     <div className="App">
-      <div className="side-bar flex-column-start-center">
-        side
+      <div className="side-bar">
+        <div className="side-bar-top">
+          top
+        </div>
+        <div className="side-bar-middle">
+          middle
+        </div>
+        <div className="side-bar-bottom">
+          bottom
+        </div>
       </div>
       <div className="main-content">
         <div className="header flex-column-center">
-          header
+          <Stories/>
         </div>
         <div className="body flex-column-center">
           body
---
+++ src/components/stories.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Stories(props) {
+
+  
+
+  return (
+    <div className="stories-container">
+      STORIES
+    </div>
+  )
+}
+
+export default Stories;
+
