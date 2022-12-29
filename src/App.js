a116c82c41e77e509606d19c3e4036cfda8fa351
Nathan Chow <emailme@nathanchow.ca>
2022-12-29 14:33:41 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="side-bar flex-column-start-center">
+        side
+      </div>
+      <div className="main-content">
+        <div className="header">
+          <div className="header-content flex-column-center">
+            header
+          </div>
+        </div>
+        <div className="body">
+          <div className="body-content flex-column-center">
+            Body
+          </div>
+        </div>
+        <div className="footer">
+          <div className="footer-content flex-column-center">
+            footer
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
