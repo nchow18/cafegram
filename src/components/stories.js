84d6825ac7a818876bb762570fe0e6657ce38301
Nathan Chow <emailme@nathanchow.ca>
2022-12-29 16:45:18 -0500

    update

--- src/components/stories.js
+++ src/components/stories.js
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 
 function Stories(props) {
 
-  
+  const [isStories, setStories] = useState([0,1,2,3,4,5,6,7,8,9,10])
+
+  const stories = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+
+  console.log(stories);
 
   return (
     <div className="stories-container">
-      STORIES
+      {stories.map((number, index) => (
+        <div key={number} className="user-story">{number}</div>
+      ))}
     </div>
   )
 }
